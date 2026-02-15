b5a7404455855ac83b8e878310f8b8b82c365161
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-02-16 01:11:56 +0500

    feat: footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,17 +2,18 @@ import React from "react";
 
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
-import { HomePage } from "./screens/homePage";
-import { ProductsPage } from "./screens/productsPage";
-import { OrdersPage } from "./screens/ordersPage";
-import { UserPage } from "./screens/userPage";
+import  HomePage  from "./screens/homePage";
+import  ProductsPage  from "./screens/productsPage";
+import  OrdersPage  from "./screens/ordersPage";
+import  UserPage  from "./screens/userPage";
 import { log } from "node:console";
-import { HomeNavbar } from "./components/headers/HomeNavbar";
-import { OtherNavbar } from "./components/headers/OtherNavbar";
-import { Footer } from "./components/footer";
+import  HomeNavbar  from "./components/headers/HomeNavbar";
+import  OtherNavbar  from "./components/headers/OtherNavbar";
+import Footer from "./components/footer";
 import "../css/app.css";
 import "../css/navbar.css";
-import { HelpPage } from "./screens/helpPage";
+import "../css/footer.css";
+import  HelpPage  from "./screens/helpPage";
 
 function App() {
   const location = useLocation();
@@ -31,7 +32,7 @@ function App() {
         <Route path="/member-page">
           <UserPage />
         </Route>
-         <Route path="/help">
+        <Route path="/help">
           <HelpPage />
         </Route>
         <Route path="/">

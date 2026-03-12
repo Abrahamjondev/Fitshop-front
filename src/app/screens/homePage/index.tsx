e66f4395de49719cb65dfab138935ce279d77bbd
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-03-12 11:50:05 +0500

    feat: create homepage Redux configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import Advertisement from "./Advirtesement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
-import "../../../css/home.css"
+import "../../../css/home.css";
+
 
 export default function HomePage() {
+  //Selector: Store => Data [yani selector biz storega saqlagan datani oladis ]
+  useEffect(() => {
+    //Backend server data request =>DATA 
+
+    //slice: Data => Store [yani backendan olingan malumot birinchi redux store ga joylanadi
+    //  keyin undan olib kelip ishlatiladi ]
+  }, []);
+
   return (
-  <div className={"homepage"}> {/**Barcha section complar homepage schreen components ichida joylashtirildi sababi ular faqat HomePage schreen comp ichida foydalaniladi */}
-    <Statistics/>
-    <PopularDishes/>
-    <NewDishes/>
-    <Advertisement/>
-    <ActiveUsers/>
-    <Events/>
-  </div>
+    <div className={"homepage"}>
+      {" "}
+      {/**Barcha section complar homepage schreen components ichida joylashtirildi sababi ular faqat HomePage schreen comp ichida foydalaniladi */}
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
   );
 }

2cd4125dd3767693901347098138c6fd20531e8f
Abrahamjondev <ibrohimjon0425@gmail.com>
2026-07-01 14:59:19 +0500

    feat: deploy project via shell and pm2

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#production
+git reset --hard
+git checkout master
+git pull origin master
+
+npm i yarn -g
+yarn global add serve
+yarn 
+yarn run build
+pm2 start "yarn run start:prod" --name=FITSHOP-FRONT
+
+#DEVELOPMENT
+# git reset --hard
+# git checkout develop
+# git pull origin develop  
+# npm i
+# pm2 start "npm run start:dev" --name=FITSHOP
